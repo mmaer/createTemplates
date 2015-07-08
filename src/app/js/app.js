8a5413078593df324ac2b4287b0eb43ebfd5822c
Kamil Kruczek <kamilkruczek0@hotmail.com>
2015-07-08 22:55:12 +0200

    0.1.0v

---
+++ src/app/js/app.js
@@ -0,0 +1,49 @@
+(function () {
+
+var CT = CT || {};
+
+CT = {
+
+	counterFlag: false,
+
+	init: function () {
+
+		this.canvas = new fabric.Canvas('createtemplate');
+
+	},
+
+	addFolderImages: function () {
+
+	},
+
+	addTemplateImage: function () {
+
+	},
+
+	setCounter: function (startNumber) {
+
+	},
+
+	setHeightTemplate: function (heightTemplate) {
+
+	},
+
+	setWidthTemplate: function (widthTemplate) {
+
+	},
+
+	setMarginTop: function (marginTop) {
+
+	},
+
+	setMarginLeft: function (marginLeft) {
+
+	}
+
+};
+
+//CT.init()
+
+//console.log(CT.init());
+
+}());
